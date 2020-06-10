58339d1144e82afd34669fe013883d99aa5f47c1
Michal Kruszewski <kruszewski2@gmail.com>
2020-06-10 16:03:14 +0200

    scss: styles added

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,8 @@ const container = document.querySelector(".container--js");
 var containerHTML = "";
 
 function createArticle(name, url){
-  var repo_description = `<p class='repo_description'><a href='${url}'>Click here to view repository online</a></p>`;
-  var repo_header = `<header class='repo_header'><h3>${name}</h3></header>`;
+  var repo_description = `<p class='repo_description'><a href='${url}' class='repo_url' target='blank'>Click here to view repository online</a></p>`;
+  var repo_header = `<header class='repo_header'><p class='repo_header--paragraph'>${name}</p></header>`;
   var article = `<article class='repo'>${repo_header}${repo_description}</article>`;
   containerHTML += article;
 }
