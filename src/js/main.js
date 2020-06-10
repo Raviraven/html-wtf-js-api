b159c1a60381cf344c698981e0885840ea249bd7
Michal Kruszewski <kruszewski2@gmail.com>
2020-06-10 15:36:43 +0200

    Fetching repos as articles implemented

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,21 @@
 "use strict";
 
-const js_api = document.querySelector(".js-api");
+const container = document.querySelector(".container--js");
+var containerHTML = "";
+
+function createArticle(name, url){
+  containerHTML += `<article class='repo'><header class='repo_header'><h3>${name}</h3></header><p class='repo_description'><a href='${url}'>Click here to view repository online</a></p></article>`;
+}
 
 fetch("https://api.github.com/users/raviraven/repos")
   .then((response) => response.json())
   .then((response) => {
     for (const repo of response) {
-      js_api.innerHTML += `<li><b>${repo.name}</b>  ${repo.html_url}</li>`;
+      createArticle(repo.name, repo.html_url);
+      console.log(repo);
+      console.log('jaidias')
     }
+    container.innerHTML = containerHTML;
   })
   .catch((error) => {
     console.log(error);
