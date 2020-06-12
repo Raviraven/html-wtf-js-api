5c1776701c8f04a5882c5c0fadd4b7b30322383b
Michal Kruszewski <kruszewski2@gmail.com>
2020-06-12 10:45:37 +0200

    scss: classes refactored

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,8 @@ const container = document.querySelector(".container--js");
 var containerHTML = "";
 
 function createArticle(name, url){
-  var repo_description = `<p class='repo_description'><a href='${url}' class='repo_url' target='blank'>Click here to view repository online</a></p>`;
-  var repo_header = `<header class='repo_header'><p class='repo_header--paragraph'>${name}</p></header>`;
+  var repo_description = `<p class='repo__description'><a href='${url}' class='repo__url' target='blank'>Click here to view repository online</a></p>`;
+  var repo_header = `<header class='repo__header'><p class='repo__header--paragraph'>${name}</p></header>`;
   var article = `<article class='repo'>${repo_header}${repo_description}</article>`;
   containerHTML += article;
 }
